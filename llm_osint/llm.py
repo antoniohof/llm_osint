e590d9cd5b6a49eeb2870b0fd357fb31c79ab99e
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2025-03-22 21:35:37 +0100

    Update llm.py
    use deepseek

--- llm_osint/llm.py
+++ llm_osint/llm.py
@@ -1,17 +1,18 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chat_models.base import BaseChatModel
+from langchain_deepseek import ChatDeepSeek
 
 LLMModel = BaseChatModel
 
-default_fast_llm_options = dict(model_name="gpt-4o-mini", request_timeout=120, max_retries=10, temperature=0.7)
-default_llm_options = dict(model_name="gpt-4o", request_timeout=120, max_retries=10, temperature=0.7)
+default_fast_llm_options = dict(model_name="deepseek-chat", request_timeout=120, max_retries=10, temperature=0.7)
+default_llm_options = dict(model_name="deepseek-chat", request_timeout=120, max_retries=10, temperature=0.7)
 
 
 def get_default_fast_llm() -> LLMModel:
-    chat = ChatOpenAI(**default_fast_llm_options)
+    chat = ChatDeepSeek(**default_fast_llm_options)
     return chat
 
 
 def get_default_llm() -> LLMModel:
-    chat = ChatOpenAI(**default_llm_options)
+    chat = ChatDeepSeek(**default_llm_options)
     return chat
